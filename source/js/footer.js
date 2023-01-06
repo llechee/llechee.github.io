94f12b3392f379737f72e391348a54cf3be45914
zhezheup <2543012055@qq.com>
2023-01-06 11:46:53 +0800

    page

---
+++ source/js/footer.js
@@ -0,0 +1,11 @@
+document.write('<!-----------  footer----------->');
+document.write('' +
+    '<center>\n' +
+    '<p style="color:#333;">© 2019-20xx Leechee</p>\n' +
+    // '<p style="color:#333;">已备案</p>\n' +
+    '</center>\n' +
+    '');
+
+
+
+
